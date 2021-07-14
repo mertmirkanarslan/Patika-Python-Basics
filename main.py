da878125b84f3c6f7d8f80ce5c9d84850972b89b
Mert Mirkan Arslan <mertmirkanarslan@gmail.com>
2021-07-14 08:15:20 +0300

    Problems are solved.

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
+# First question is about flatten function.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print("Project Part 1 : Write a flatten function")
+print("------------------------------------")
+my_input = [[1, 'a', ['cat'], 2], [[[3]], 'dog'], 4, 5, [[[['dinosaur']]], 19]]
+print("Our input is: " + str(my_input))
+# We should create an empty list for flatten list.
+EmptyList = []
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def flatten(x):
+    for i in x:
+        if type(i) != list:
+            EmptyList.append(i)
+        else:
+            flatten(i)
+    return EmptyList
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print("The result is:" + str(flatten(my_input)))
+print("Part 1 is over.")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# Second question is about reversal list
+
+print("Project Part 2 : Write a reverse function")
+print("-------------------------------------------")
+
+my_array = [[1, 2], 3, [4, 5], [6, 7, 8], [9, 10, [11, 12]], [14, 13, 20, [25, 16, [17, 33, 19]]]]
+print("Our input array is:" + str(my_array))
+
+
+def reverse_array(my_array):
+    my_array.reverse()
+    for arr in my_array:
+        if(type(arr) == list):
+            reverse_array(arr)
+
+
+reverse_array(my_array)
+reverse_array(my_input)
+print("The result is:" + str(my_array))
+print("We can try this function with the first question's input too:")
+print("First Question's Result for Question 2 is:" + str(my_input))
+print("Part 2 is over. Program is terminating...")
